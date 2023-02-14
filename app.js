2af5ddad733d9a10352ec829e0ebb33d1fee9d83
Blackstar84 <carlosalmada84@gmail.com>
2023-02-13 21:39:30 -0300

    asdasd

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    for (let i = 1; i <= 50; i++) {
+        if(i%2!==0){
+            res.send(`<p>${i} Soy Impar!</p>`);
+        }else{
+            res.send(`<p>${i} Soy Par!</p>`);
+        }
+        
+    }
+  
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
