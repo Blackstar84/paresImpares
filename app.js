399af71dcd87d11ccee0194a496597309b55c56c
Blackstar84 <carlosalmada84@gmail.com>
2023-02-14 07:52:27 -0300

    se finaliza el par e impar

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    let word = "";
     for (let i = 1; i <= 50; i++) {
-        if(i%2!==0){
-            res.send(`<p>${i} Soy Impar!</p>`);
+        if(i%2!==0){            
+            res.write(`<p>${i} Soy Impar!</p>`);
         }else{
-            res.send(`<p>${i} Soy Par!</p>`);
+            res.write(`<p>${i} Soy Par!</p>`);
         }
         
     }
+    res.end();
   
 });
 
